8f12cddc1ec464f2b5d225c1b25991518e2aff1c
Joey Gauthier <digitaljedi@outlook.com>
2018-07-07 14:16:40 -0500

    stubbing out more services for now

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -8,6 +8,7 @@ var auth = jwt({
 
 var ctrlProfile = require('../controllers/profile');
 var ctrlAuth = require('../controllers/authentication');
+var ctrlPhotos = require('../controllers/photos');
 
 // profile
 router.get('/profile', auth, ctrlProfile.profileRead);
@@ -15,5 +16,17 @@ router.get('/profile', auth, ctrlProfile.profileRead);
 // authentication
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
+
+// photos
+router.get('/photos/list', ctrlPhotos.getList);
+router.post('/photos/upload', ctrlPhotos.uploadPhotos);
+router.patch('/photos/delete', ctrlPhotos.deletePhoto);
+
+// admin
+// get users - get list of users
+// get user - get specific user
+// delete user - remove user
+// patch user - suspend account for a while and/or modify user
+//
 
 module.exports = router;
