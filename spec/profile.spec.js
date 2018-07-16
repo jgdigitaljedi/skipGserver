39700abc2fea5d2edcc677de1092de3de6610ad6
Joey Gauthier <jgauthier@r4.co>
2018-07-16 08:17:13 -0500

    used beforeAll to fetch and store user data and header for remaining calls

--- spec/profile.spec.js
+++ spec/profile.spec.js
@@ -1,27 +1,35 @@
 const frisby = require('frisby');
 const common = require('./common');
 
+
 describe('Profile', function () {
-	it('should fetch user info, verify it gets 3 keys back and that admin is false', function () {
-		return global.login().then((res) => {
-			const user = res.data;
-			const headers = {
-				Authorization: `Bearer ${user.token}`
-			};
-			return frisby
-				.fetch(`${common.baseUrl}profile`, {
-					method: 'GET',
-					headers
-				})
-				.expect('status', 200)
-				.then((data) => {
-					const parsed = JSON.parse(data.body);
-					expect(Object.keys(parsed).length).toEqual(3);
-					expect(parsed.admin).toBeFalsy();
-				});
-		})
+	let user;
+	let headers;
+	beforeAll((done) => {
+		global.login()
+			.then((res) => {
+				user = res.data;
+				headers = {
+					Authorization: `Bearer ${user.token}`
+				};
+				done();
+			})
 			.catch((err) => {
-				console.log('profile error', err);
+				console.error('Profile beforeAll promise rejected', err);
+			});
+	});
+
+	it('should fetch user info, verify it gets 3 keys back and that admin is false', function () {
+		return frisby
+			.fetch(`${common.baseUrl}profile`, {
+				method: 'GET',
+				headers
+			})
+			.expect('status', 200)
+			.then((data) => {
+				const parsed = JSON.parse(data.body);
+				expect(Object.keys(parsed).length).toEqual(3);
+				expect(parsed.admin).toBeFalsy();
 			});
 	});
 });
