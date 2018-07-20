22f3d941643d922fb6660a82aa79c234cc43a42e
Joey Gauthier <jgauthier@r4.co>
2018-07-20 07:30:59 -0500

    cleared git cache and removed docs from tracking

--- docs/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "Skipg.me backend",  "version": "0.1.0",  "description": "Documentation of API",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-07-20T12:27:35.909Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
