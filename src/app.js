6b4932e40d241a643fff344434c6ed2fb3d2e6d3
Joey Gauthier <jgauthier@r4.co>
2018-07-20 08:21:11 -0500

    fixed app.js next called but not defined

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(passport.initialize());
 app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
-app.use((req, res) => {
+app.use((req, res, next) => {
 	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
