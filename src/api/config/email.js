f1ad26b6650c9f973ef93c0d987c810127615fa8
Joey Gauthier <digitaljedi@outlook.com>
2018-07-25 06:36:38 -0500

    email.js has conditional links for env, started Docker setup

--- src/api/config/email.js
+++ src/api/config/email.js
@@ -6,7 +6,9 @@ const chalk = require('chalk');
 module.exports.sendEmail = function (email, name, token) {
   return new Promise((resolve, reject) => {
     let recipient;
+    let baseUrl;
     if (process.env.NODE_ENV === 'production') {
+      baseUrl = 'https://skipg.me/';
       recipient = {
         address: {
           name: `${name.first} ${name.last}`,
@@ -14,6 +16,7 @@ module.exports.sendEmail = function (email, name, token) {
         }
       };
     } else {
+      baseUrl = 'http://localhost:4200/';
       recipient = {
         address: {
           name: 'Joey G',
@@ -31,7 +34,9 @@ module.exports.sendEmail = function (email, name, token) {
           name: 'Test'
         },
         subject: 'Skipg.me password reset requested',
-        html: `<html><body>Click on this link to reset your password: https://skipg.me/api/user/resetlink/${token}</body></html>`
+        // this link should be handled by the Angular router to send the token to an endpoint, find user by reset token, check to make sure
+        // it is not expired, then redirect them to a password change page
+        html: `<html><body>Click on this link to reset your password: ${baseUrl}resetlink/${token}</body></html>`
       },
       recipients: [
         recipient
